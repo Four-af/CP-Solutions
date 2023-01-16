cb8ff9db92d31408a443897b61c2a086f16143e3
Aiman Fatima <83394414+Four-af@users.noreply.github.com>
2023-01-17 00:26:10 +0530

    Committing files

---
+++ CodeChef/C++17/SEMCOURSES/63741219.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+#define vll vector<ll>
+#define ll long long
+
+//////////////////////////////////////////////////////
+//////////////////////////////////////////////
+
+int main()
+{
+    ll testmeee;
+    cin >> testmeee;
+    for (ll kkk = 0; kkk < testmeee; kkk++)
+    {
+        ll noellee,yelan,zhonglee;
+        cin >> noellee>>yelan>>zhonglee;
+        std::cout << noellee*yelan*zhonglee << std::endl;
+        
+    }
+    return 0;
+}
