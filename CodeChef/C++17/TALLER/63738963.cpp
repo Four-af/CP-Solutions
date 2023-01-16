8f898838458a3f1e97bd38d371d267ddc29ce6b7
Aiman Fatima <83394414+Four-af@users.noreply.github.com>
2023-01-17 00:26:16 +0530

    Committing files

---
+++ CodeChef/C++17/TALLER/63738963.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+#define vll vector<ll>
+#define ll long long
+
+//////////////////////////////////////////////////////
+//////////////////////////////////////////////
+
+int main()
+{
+    ll testmeee;
+    cin >> testmeee;
+    for (ll kkk = 0; kkk < testmeee; kkk++)
+    {
+        ll noellee,boii;
+        cin >> noellee>>boii;
+        if(noellee>boii) std::cout << "a" << std::endl;
+        else cout<<"b\n";
+        
+    }
+    return 0;
+}
