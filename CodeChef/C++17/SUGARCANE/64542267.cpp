8517df10d43a0ba19b38b4b434ac1f48bd982138
Aiman Fatima <83394414+Four-af@users.noreply.github.com>
2023-01-17 00:24:44 +0530

    Committing files

---
+++ CodeChef/C++17/SUGARCANE/64542267.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vll vector<ll>
+#define vch vector<char>
+
+//////////////////////////////////////////////////////
+//////////////////////////////////////////////
+int main()
+{
+    ll testmeee;
+    cin >> testmeee;
+    for (ll kkk = 0; kkk < testmeee; kkk++)
+    {
+        ll noellee;
+        cin >> noellee;
+        cout<<noellee*(20-5)<<"\n";
+        // 2345521
+        // 11 000 1111 00000 11111 00 1
+        // 4
+        // time_req = clock() - time_req;
+        // cout << "\n:: " << (float)time_req / CLOCKS_PER_SEC;
+        // 11 000 0110 00000 11111 11 1
+    } // n/n-4
+    return 0;
+}
+
+// 5000*
